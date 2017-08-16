42c1a44918af981140e92f1876283cb08cbe9fcf
Felipe Arosemena <felipe@briteweb.com>
2017-08-16 20:37:37 +0800

    remove modal from nav, now state gets controlled via store

--- wp-content/themes/zephyr/src/scripts/app/nav.js
+++ wp-content/themes/zephyr/src/scripts/app/nav.js
@@ -3,36 +3,19 @@ import Vue from 'vue'
 import router from 'app/router'
 import store from 'app/store'
 
-import { toggleClass } from 'modules/utils'
-import { initModal } from 'modules/modal'
-
 export default function NavFactory(el) {
 
   const nav = new Vue({
     el: el,
     router: router,
     data: store.state,
-    mounted() {
-      this.toggle = toggleClass(document.body, 'is-nav-active')
-      this.modal = initModal(document.querySelector('#site-nav'))
-      this.modal.subscribe('open', () => this.onOpen())
-      this.modal.subscribe('close', () => this.onClose())
-    },
     methods: {
-      toggleCart: bool => store.toggleCart(bool),
-      onOpen() {
-        this.toggle(true)
-        store.setState({ navOpen: true })
-      },
-      onClose() {
-        this.toggle(false)
-        store.setState({ navOpen: false })
-      },
+      toggleCart: () => store.toggleCart(),
+      toggleNav: () => store.toggleNav()
     }
   })
 
   router.beforeEach((to, from, next) => {
-    nav.modal.close()
     next()
   })
 
