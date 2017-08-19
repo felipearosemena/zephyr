e2c18a1a4ac69d381406e07824a9495397f15246
Felipe Arosemena <felipe@briteweb.com>
2017-08-19 14:13:14 +0800

    remove router watch handler code, not in use at this stage

--- wp-content/themes/zephyr/src/scripts/app/router.js
+++ wp-content/themes/zephyr/src/scripts/app/router.js
@@ -2,12 +2,8 @@ import VueRouter from 'vue-router'
 
 const router = new VueRouter({
   mode: 'history',
-  watch: { 
-    '$route' (to, from) {
-      const toDepth = to.path.split('/').length
-      const fromDepth = from.path.split('/').length
-      this.transitionName = toDepth < fromDepth ? 'slide-right' : 'slide-left'
-    }
+  watch: {
+    '$route' (to, from) {}
   }
 })
 
