643132e8ca4c6d928b2193d63a0b402f36daf1c0
Felipe Arosemena <felipe@briteweb.com>
2017-08-19 20:58:19 +0800

    remove getting posts and categories from template on shop, will be using rest api instead

--- wp-content/themes/zephyr/woocommerce.php
+++ wp-content/themes/zephyr/woocommerce.php
@@ -17,13 +17,7 @@ if (is_singular('product')) {
 
 } else {
 
-  $posts = Timber::get_posts();
-  $context['products'] = $posts;
-
   if ( is_product_category() ) {
-    $queried_object = get_queried_object();
-    $term_id = $queried_object->term_id;
-    $context['category'] = get_term( $term_id, 'product_cat' );
     $context['title'] = single_term_title('', false);
   }
 
