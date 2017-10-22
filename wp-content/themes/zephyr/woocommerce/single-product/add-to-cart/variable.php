ca397b9a0e42e1df58704b7207d4ba4464c84c61
Felipe Arosemena <felipe@briteweb.com>
2017-10-22 11:43:45 -0700

    update add to cart form on single

--- wp-content/themes/zephyr/woocommerce/single-product/add-to-cart/variable.php
+++ wp-content/themes/zephyr/woocommerce/single-product/add-to-cart/variable.php
@@ -33,40 +33,52 @@ do_action( 'woocommerce_before_add_to_cart_form' ); ?>
     <p class="stock out-of-stock"><?php _e( 'This product is currently out of stock and unavailable.', 'woocommerce' ); ?></p>
   <?php else : ?>
 
-    <?php foreach ( $attributes as $attribute_name => $options ) : ?>
-      <label for="<?php echo sanitize_title( $attribute_name ); ?>"><?php echo wc_attribute_label( $attribute_name ); ?></label>
+    <div class="grid">
+      <?php foreach ( $attributes as $attribute_name => $options ) : ?>
+        <div class="grid__item w-4-12">
+          <label for="<?php echo sanitize_title( $attribute_name ); ?>" class="sr-only"><?php echo wc_attribute_label( $attribute_name ); ?></label>
+
+          <div class="select-wrapper">
+            <?php
+            $selected = isset( $_REQUEST[ 'attribute_' . sanitize_title( $attribute_name ) ] ) ?
+            wc_clean( stripslashes( urldecode( $_REQUEST[ 'attribute_' . sanitize_title( $attribute_name ) ] ) ) ) :
+            $product->get_variation_default_attribute( $attribute_name );
+            wc_dropdown_variation_attribute_options( array(
+              'options' => $options,
+              'attribute' => $attribute_name,
+              'product' => $product,
+              'selected' => $selected,
+              'show_option_none' => wc_attribute_label( $attribute_name )
+            ));
+            ?>
+          </div>
+        </div>
+      <?php endforeach;?>
+
+      <?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+
       <?php
-        $selected = isset( $_REQUEST[ 'attribute_' . sanitize_title( $attribute_name ) ] ) ? wc_clean( stripslashes( urldecode( $_REQUEST[ 'attribute_' . sanitize_title( $attribute_name ) ] ) ) ) : $product->get_variation_default_attribute( $attribute_name );
-        wc_dropdown_variation_attribute_options( array( 'options' => $options, 'attribute' => $attribute_name, 'product' => $product, 'selected' => $selected ) );
+      /**
+      * woocommerce_before_single_variation Hook.
+      */
+      do_action( 'woocommerce_before_single_variation' );
 
+      /**
+      * woocommerce_single_variation hook. Used to output the cart button and placeholder for variation data.
+      * @since 2.4.0
+      * @hooked woocommerce_single_variation - 10 Empty div for variation data.
+      * @hooked woocommerce_single_variation_add_to_cart_button - 20 Qty and cart button.
+      */
+      do_action( 'woocommerce_single_variation' );
+
+      /**
+      * woocommerce_after_single_variation Hook.
+      */
+      do_action( 'woocommerce_after_single_variation' );
       ?>
-    <?php endforeach;?>
 
-    <?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
-
-    <div class="single_variation_wrap">
-      <?php
-        /**
-         * woocommerce_before_single_variation Hook.
-         */
-        do_action( 'woocommerce_before_single_variation' );
-
-        /**
-         * woocommerce_single_variation hook. Used to output the cart button and placeholder for variation data.
-         * @since 2.4.0
-         * @hooked woocommerce_single_variation - 10 Empty div for variation data.
-         * @hooked woocommerce_single_variation_add_to_cart_button - 20 Qty and cart button.
-         */
-        do_action( 'woocommerce_single_variation' );
-
-        /**
-         * woocommerce_after_single_variation Hook.
-         */
-        do_action( 'woocommerce_after_single_variation' );
-      ?>
+      <?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
     </div>
-
-    <?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
   <?php endif; ?>
 
   <?php do_action( 'woocommerce_after_variations_form' ); ?>
--- wp-content/themes/zephyr/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ wp-content/themes/zephyr/woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -13,26 +13,35 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $product;
 ?>
-<div class="woocommerce-variation-add-to-cart variations_button">
+
+<div class="grid__item w-3-12">
   <?php
     /**
      * @since 3.0.0.
      */
     do_action( 'woocommerce_before_add_to_cart_quantity' );
+  ?>
 
-    woocommerce_quantity_input( array(
-      'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
-      'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
-      'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( $_POST['quantity'] ) : $product->get_min_purchase_quantity(),
-    ) );
+    <div class="select-wrapper">
+      <select name="quantity">
+        <?php
+        for ($i=1; $i < 7; $i++) {
+          echo '<option value=' . $i . '>' . $i . '</option>';
+        }
+        ?>
+      </select>
+    </div>
 
+  <?php
     /**
      * @since 3.0.0.
      */
     do_action( 'woocommerce_after_add_to_cart_quantity' );
   ?>
+</div>
 
-  <button type="submit" class="btn" ref="submit" :disabled="!state.product.canAddToCart"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+<div class="grid__item w-5-12">
+  <button type="submit" class="btn btn--full" ref="submit" :disabled="!state.product.canAddToCart"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
   <input type="hidden" name="add-to-cart" value="<?php echo absint( $product->get_id() ); ?>" />
   <input type="hidden" name="product_id" value="<?php echo absint( $product->get_id() ); ?>" />
   <input type="hidden" name="variation_id" class="variation_id" value="0" />
