e1efe2968c4ea746393d543e86cb29ebd24ecd96
Felipe Arosemena <felipe@briteweb.com>
2017-11-19 18:39:10 -0800

    add isLoading watcher for cart component

--- wp-content/themes/zephyr/src/scripts/app/cart.component.js
+++ wp-content/themes/zephyr/src/scripts/app/cart.component.js
@@ -1,9 +1,23 @@
 import store from 'app/store'
+import { mapObject } from 'modules/utils'
 
 const Cart = {
   template: require('templates/cart.component.html'),
   props: [ 'cart', 'is-active', 'is-loading', 'is-checkout', 'products' ],
   mounted() {},
+  data() {
+    return {
+      subtotal: ''
+    }
+  },
+  watch: {
+    isLoading() {
+      this.subtotal = mapObject(
+        this.cart.contents,
+        (k, { line_subtotal }) => line_subtotal
+      ).reduce((curr, next) => curr + next, 0)
+    }
+  },
   methods: {
     toggleCart: () => store.toggleCart()
   }
