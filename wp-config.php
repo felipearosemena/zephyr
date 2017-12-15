21d6b3adea29e97113ff6bcfdccff80ef59b6faf
Felipe Arosemena <felipe@briteweb.com>
2017-12-15 14:55:58 -0800

    add force ssl & domain redirection to wp-config

--- wp-config.php
+++ wp-config.php
@@ -25,6 +25,30 @@ if (file_exists(dirname(__FILE__) . '/wp-config-local.php') && !isset($_ENV['PAN
 else:
   if (isset($_ENV['PANTHEON_ENVIRONMENT'])):
 
+    if (isset($_SERVER['PANTHEON_ENVIRONMENT']) && php_sapi_name() != 'cli') {
+      // Redirect to https://$primary_domain in the Live environment
+      if ($_ENV['PANTHEON_ENVIRONMENT'] === 'live') {
+        $primary_domain = 'zephyrjewelry.com';
+      } else {
+        // Redirect to HTTPS on every Pantheon environment.
+        $primary_domain = $_SERVER['HTTP_HOST'];
+      }
+
+      if ($_SERVER['HTTP_HOST'] != $primary_domain
+          || !isset($_SERVER['HTTP_X_SSL'])
+          || $_SERVER['HTTP_X_SSL'] != 'ON' ) {
+
+        # Name transaction "redirect" in New Relic for improved reporting (optional)
+        if (extension_loaded('newrelic')) {
+          newrelic_name_transaction("redirect");
+        }
+
+        header('HTTP/1.0 301 Moved Permanently');
+        header('Location: https://'. $primary_domain . $_SERVER['REQUEST_URI']);
+        exit();
+      }
+    }
+
     if (!in_array($_ENV['PANTHEON_ENVIRONMENT'], array('test', 'live'))) {
 
       // Debugging enabled.
