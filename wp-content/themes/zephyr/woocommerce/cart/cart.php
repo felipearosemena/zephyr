20efd61bfef25bc7d604290faff57873d7f1d30e
Felipe Arosemena <felipe@briteweb.com>
2017-12-15 15:08:28 -0800

    update cart page markup

--- wp-content/themes/zephyr/woocommerce/cart/cart.php
+++ wp-content/themes/zephyr/woocommerce/cart/cart.php
@@ -134,12 +134,14 @@ do_action( 'woocommerce_before_cart' ); ?>
 
           <?php if ( wc_coupons_enabled() ) { ?>
             <div class="coupon">
-              <label for="coupon_code"><?php _e( 'Coupon:', 'woocommerce' ); ?></label> <input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" /> <input type="submit" class="button" name="apply_coupon" value="<?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?>" />
+              <label for="coupon_code"><?php _e( 'Coupon:', 'woocommerce' ); ?></label>
+              <input type="text" name="coupon_code" class="input-text" id="coupon_code" value="" placeholder="<?php esc_attr_e( 'Coupon code', 'woocommerce' ); ?>" />
+              <button type="submit" class="btn" name="apply_coupon"><?php esc_attr_e( 'Apply coupon', 'woocommerce' ); ?></button>
               <?php do_action( 'woocommerce_cart_coupon' ); ?>
             </div>
           <?php } ?>
 
-          <input type="submit" class="button" name="update_cart" value="<?php esc_attr_e( 'Update cart', 'woocommerce' ); ?>" />
+          <button type="submit" class="btn" name="update_cart"><?php esc_attr_e( 'Update cart', 'woocommerce' ); ?></button>
 
           <?php do_action( 'woocommerce_cart_actions' ); ?>
 
