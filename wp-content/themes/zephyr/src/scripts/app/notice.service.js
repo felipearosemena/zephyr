655949d595bfef1a45d7a24f2c4bfbedc7879993
Felipe Arosemena <felipe@briteweb.com>
2017-12-15 20:46:50 -0800

    add notice service

---
+++ wp-content/themes/zephyr/src/scripts/app/notice.service.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import store from 'app/store'
+import { apiFetch } from 'modules/load'
+
+const NoticeService = new Vue({
+  data: {
+    notices: {}
+  },
+
+  methods: {
+    getNotices() {
+      apiFetch(Global.api_namespace + '/notices')
+        .then(res => this.hydrate(res))
+    },
+
+    hydrate(res) {
+
+      return res.json().then(notices => {
+
+        store.setState({ notices: notices })
+
+      })
+
+    }
+  }
+
+})
+
+export default NoticeService
